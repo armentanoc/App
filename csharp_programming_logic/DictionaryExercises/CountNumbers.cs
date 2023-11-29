9d43dfaf9871c48f0c9826e12d55738f41dc7adf
armentanoc <armentanocarolina@gmail.com>
2023-11-29 10:54:47 -0300

    Counts the occurrences of numbers in an array and puts them in a dictionary.

--- csharp_programming_logic/DictionaryExercises/CountNumbers.cs
+++ csharp_programming_logic/DictionaryExercises/CountNumbers.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-/*
- Exercício de Dicionário 1 - Dado um array de inteiros determine qual número que mais se repete:
-Array: 
-int[] arrayDeInteiros = new int[] { 1, 1, 8, 8, 0, 5, 5, 5, 8, 3, 8, 3, 10 }
-NOTA: Use dicionário para isso!!!
- */
+﻿using LibFormatOutput;
 
 namespace csharp_programming_logic.DictionaryExercises
 {
@@ -16,7 +6,30 @@ namespace csharp_programming_logic.DictionaryExercises
     {
         internal static void Execute()
         {
-            throw new NotImplementedException();
+            PrintTitle();
+
+            int[] arrayDeInteiros = new int[] { 1, 1, 8, 8, 0, 5, 5, 5, 8, 3, 8, 3, 10 };
+            
+            Dictionary<int, int> occurrences = arrayDeInteiros
+                .GroupBy(n => n) //O método GroupBy em LINQ é usado para agrupar elementos de uma sequência com base em uma chave comum
+                .ToDictionary(g => g.Key, g => g.Count()); //
+
+            Console.WriteLine("Array\n");
+            foreach (int numero in arrayDeInteiros) {
+                Console.Write(numero + " ");
+            }
+
+            Console.WriteLine("\n\nNúmero de ocorrências\n");
+            foreach (var entry in occurrences)
+            {
+                Console.WriteLine($"Chave {entry.Key} => Valor {entry.Value}");
+            }
+        }
+
+        private static void PrintTitle()
+        {
+            Console.WriteLine("\nCONTAGEM DE NÚMEROS VIA DICIONÁRIO");
+            Print.HorizontalLine();
         }
     }
 }
