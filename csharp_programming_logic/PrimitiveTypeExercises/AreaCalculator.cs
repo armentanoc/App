fa6fcd80289096663b85f87bd37b0f61bb7ad044
armentanoc <armentanocarolina@gmail.com>
2023-11-29 10:25:45 -0300

    Finalizing primitive type exercises.

--- csharp_programming_logic/PrimitiveTypeExercises/AreaCalculator.cs
+++ csharp_programming_logic/PrimitiveTypeExercises/AreaCalculator.cs
@@ -1,11 +1,35 @@
 ﻿
+using LibFormatOutput;
+using LibUserInput;
+
 namespace csharp_programming_logic.PrimitiveTypeExercises
 {
     internal class AreaCalculator
     {
         internal static void Execute()
         {
-            throw new NotImplementedException();
+            PrintTitle();
+            Console.WriteLine("Informe o raio do círculo");
+
+            double PI = 3.1416F;
+            double raio = Receive.FloatNumber();
+            double area;
+
+            try
+            {
+                area = PI * Math.Pow(raio, 2) / 2;
+                Console.WriteLine($"A área do círculo é igual a {area}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Erro: {e.Message}");
+            }
+        }
+
+        static void PrintTitle()
+        {
+            Console.WriteLine("\nÁREA DO CÍRCULO");
+            Print.HorizontalLine();
         }
     }
 }
--- csharp_programming_logic/PrimitiveTypeExercises/TypeConversion.cs
+++ csharp_programming_logic/PrimitiveTypeExercises/TypeConversion.cs
@@ -4,6 +4,7 @@ namespace csharp_programming_logic.PrimitiveTypeExercises
 
     using LibUserInput;
     using LibFormatOutput;
+    using System.Numerics;
 
     internal class TypeConversion
     {
@@ -12,14 +13,39 @@ namespace csharp_programming_logic.PrimitiveTypeExercises
             PrintTitle();
 
             decimal inputNumber = Receive.Decimal();
-            int intNumber = Convert.ToInt32(inputNumber);
-            double doubleNumber = Convert.ToDouble(inputNumber);
-            float floatNumber = Convert.ToSingle(inputNumber);
+            int intNumber;
+            double doubleNumber;
+            float floatNumber;
 
-            Console.WriteLine($"\nDecimal: {inputNumber}");
-            Console.WriteLine($"Int: {intNumber}");
-            Console.WriteLine($"Double: {doubleNumber}");
-            Console.WriteLine($"Float: {floatNumber}");
+            if (int.TryParse(inputNumber.ToString(), out var intValue))
+            {
+                intNumber = intValue;
+                Console.WriteLine($"Int: {intNumber}");
+            }
+            else
+            {
+                Console.WriteLine("Não foi possível converter em int.");
+            }
+
+            if (double.TryParse(inputNumber.ToString(), out var doubleValue))
+            {
+                doubleNumber = doubleValue;
+                Console.WriteLine($"Double: {doubleNumber}");
+            }
+            else
+            {
+                Console.WriteLine("Não foi possível converter em double.");
+            }
+
+            if (float.TryParse(inputNumber.ToString(), out var floatValue))
+            {
+                floatNumber = floatValue;
+                Console.WriteLine($"Float: {floatNumber}");
+            }
+            else
+            {
+                Console.WriteLine("Não foi possível converter em float.");
+            }
         }
 
         private static void PrintTitle()
