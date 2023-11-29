7b17b36241c8c6e435186a5692b1049bdc6e876b
armentanoc <armentanocarolina@gmail.com>
2023-11-29 11:30:51 -0300

    Program that interprets the language the user wants and use a dict to print words accordingly.

--- csharp_programming_logic/DictionaryExercises/ChooseLanguage.cs
+++ csharp_programming_logic/DictionaryExercises/ChooseLanguage.cs
@@ -1,9 +1,8 @@
-﻿
-/*
- 
-Exercício de Dicionário 2 - Crie dicionários de forma que o usuário possa selecionar entre vários idiomas para exibição dos textos do programa.
-
- */
+﻿using LibFormatOutput;
+using LibUserInput;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace csharp_programming_logic.DictionaryExercises
 {
@@ -11,7 +10,58 @@ namespace csharp_programming_logic.DictionaryExercises
     {
         internal static void Execute()
         {
-            throw new NotImplementedException();
+            var translations = new Dictionary<int, Dictionary<int, string>>
+
+            {
+                [1] = new Dictionary<int, string> { [1] = "Olá", [2] = "Mundo" },
+                [2] = new Dictionary<int, string> { [1] = "Hello", [2] = "World" },
+                [3] = new Dictionary<int, string> { [1] = "Hola", [2] = "Mundo" }
+            };
+
+            PrintTitle();
+
+            while (true)
+            {
+                Console.WriteLine("\nVocê deverá selecionar um idioma para exibição do programa\n1 - Português \n2 - Inglês \n3 - Espanhol \n0 - Sair\n");
+                int languageKey = Receive.IntNumber();
+
+                if(languageKey == 0)
+                {
+                    break;
+                }
+
+                if (translations.ContainsKey(languageKey))
+                {
+                    PrintTranslations(translations.GetValueOrDefault(languageKey));
+                }
+                else
+                {
+                    Console.WriteLine("Entrada de idioma inválida. Por favor, tente novamente.");
+                }
+            }
+
+            static void PrintTranslations(Dictionary<int, string> languageDictionary)
+            {
+                if (languageDictionary is not null)
+                {
+                    foreach (var (id, translation) in languageDictionary)
+                    {
+                        Console.Write($"{translation} ");
+                    }
+
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("Idioma não suportado.");
+                }
+            }
+        }
+
+        private static void PrintTitle()
+        {
+            Console.WriteLine("\nESCOLHA DE IDIOMA PARA O PROGRAMA");
+            Print.HorizontalLine();
         }
     }
 }
